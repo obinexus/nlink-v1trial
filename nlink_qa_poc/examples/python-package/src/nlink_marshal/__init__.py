96213a52a83adaa670b000203286a60473e46e5d
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-13 23:07:57 +0100

    W

--- nlink_qa_poc/examples/python-package/src/nlink_marshal/__init__.py
+++ nlink_qa_poc/examples/python-package/src/nlink_marshal/__init__.py
@@ -5,7 +5,7 @@ Pure Python implementation of the OBINexus Mathematical Framework
 for zero-overhead data marshalling in distributed systems.
 """
 
-from .marshaller import PythonMarshaller
+from .marshaller import PythonMarshaller, create_marshaller
 from .topology import TopologyManager
 from .validation import SecurityValidator
 
@@ -16,4 +16,10 @@ __all__ = [
     "PythonMarshaller",
     "TopologyManager", 
     "SecurityValidator",
+    "create_marshaller",  # Critical export for integration tests
 ]
+
+# Provide module-level access to factory function
+def get_marshaller():
+    """Alternative factory function for marshaller creation"""
+    return create_marshaller()
